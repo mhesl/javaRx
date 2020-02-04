e55b3a868d7d8ef66e62e62f3f1e59304d0b3a22
mohammad6035 <mohammadeslami1379@yahoo.com>
2020-02-04 23:27:07 +0330

    simple observable and observer created

--- app/src/main/java/com/example/javarx/MainActivity.java
+++ app/src/main/java/com/example/javarx/MainActivity.java
@@ -3,13 +3,57 @@ package com.example.javarx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // observable
+        Observable<String> animalsObservable = getAnimalsObservable();
+        // observer
+        Observer<String> animalsObserver = getAnimalsObserver();
+        // observer subscribing to observable
+        animalsObservable
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(animalsObserver);
 
+
+    }
+
+    private Observer<String> getAnimalsObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+    private Observable<String> getAnimalsObservable() {
+        return Observable.just("Ant", "Bee", "Cat", "Dog", "Fox");
     }
 }
