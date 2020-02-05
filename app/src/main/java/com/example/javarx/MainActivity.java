391b9a0f1cdf94365a94a581ec777d5d803e098f
mohammad6035 <mohammadeslami1379@yahoo.com>
2020-02-06 01:46:48 +0330

    composite disposables and chaining of operators

--- app/src/main/java/com/example/javarx/MainActivity.java
+++ app/src/main/java/com/example/javarx/MainActivity.java
@@ -8,12 +8,15 @@ import android.util.Log;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    private Disposable disposable;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
     private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,30 +24,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // observable
         Observable<String> animalsObservable = getAnimalsObservable();
-        // observer
-        Observer<String> animalsObserver = getAnimalsObserver();
-        // observer subscribing to observable
-        animalsObservable
-                .observeOn(Schedulers.io())
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .filter(new Predicate<String>() {
-                    @Override
-                    public boolean test(String s) throws Exception {
-                        return s.toLowerCase().startsWith("b");
-                    }
-                })
-                .subscribe(animalsObserver);
+
+        DisposableObserver<String> animalsObserver = getAnimalsObserver();
+
+        DisposableObserver<String> animalsObserverAllCaps = getAnimalsAllCapsObserver();
+
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("b");
+                            }
+                        })
+                        .subscribeWith(animalsObserver));
+
+
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("c");
+                            }
+                        })
+                        .map(new Function<String, String>() {
+                            @Override
+                            public String apply(String s) throws Exception {
+                                return s.toUpperCase();
+                            }
+                        })
+                        .subscribeWith(animalsObserverAllCaps));
 
 
     }
 
-    private Observer<String> getAnimalsObserver() {
-        return new Observer<String>() {
+    private DisposableObserver<String> getAnimalsObserver() {
+        return new DisposableObserver<String>() {
+
             @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe");
-                disposable = d;
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
             }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
+    private DisposableObserver<String> getAnimalsAllCapsObserver() {
+        return new DisposableObserver<String>() {
+
 
             @Override
             public void onNext(String s) {
@@ -74,6 +115,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        disposable.dispose();
+        compositeDisposable.clear();
     }
 }
