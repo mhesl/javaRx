ddb796d930be123874d18ebfe572828123f9b88f
mohammad6035 <mohammadeslami1379@yahoo.com>
2020-02-06 21:11:52 +0330

    note class created as model

---
+++ app/src/main/java/com/example/javarx/Note.java
@@ -0,0 +1,22 @@
+package com.example.javarx;
+
+public class Note {
+    int id ;
+    String note;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
